4c4949b65003a097d7feaf1b575172f5d72dce73
Likianta <likianta@foxmail.com>
2021-10-22 10:44:08 +0800

    Fix doctor's instruction

--- build/doctor.py
+++ build/doctor.py
@@ -19,26 +19,36 @@ except Exception as e:
         You can try the following steps to fix this problem:
         
         - I'm using Windows 7 (32-bit)
-            - Go to '{depsland}/windows_patch/windows-7-sp1-32bit' folder
-            - Setup 'KB2999226-x86.msu' and 'KB2999226-x86.msu'
-            - Restart your computer
-            - Retry to setup this program
+            - The error shows 'DLL load failed...'
+                - Go to '{depsland}/windows_patch/windows-7-sp1-32bit' folder
+                - Setup 'KB2999226-x86.msu' and 'KB3063858-x86.msu'
+                - Restart your computer
+                - Retry to setup this program
+            - The error shows 'api-ms-core-... is misssing'
+                - Go to '{depsland}/windows_patch/windows-7-sp1-32bit' folder
+                - Setup 'KB2999226-x86.msu'
+                - Retry to setup this program (no need to restart computer)
             
         - I'm using Windows 7 (64-bit)
-            - Go to '{depsland}/windows_patch/windows-7-sp1-64bit' folder
-            - Setup 'KB2999226-x64.msu' and 'KB2999226-x64.msu'
-            - Restart your computer
-            - Retry to setup this program
-            
+            - The error shows 'DLL load failed...'
+                - Go to '{depsland}/windows_patch/windows-7-sp1-64bit' folder
+                - Setup 'KB2999226-x64.msu' and 'KB3063858-x64.msu'
+                - Restart your computer
+                - Retry to setup this program
+            - The error shows 'api-ms-core-... is misssing'
+                - Go to '{depsland}/windows_patch/windows-7-sp1-64bit' folder
+                - Setup 'KB2999226-x64.msu'
+                - Retry to setup this program (no need to restart computer)
+        
         - I'm using Windows 8 (32-bit)
             - Go to '{depsland}/windows_patch/windows-8-32bit' folder
             - Setup 'vcredist-2015-x86.exe'
-            - Retry to setup this program
+            - Retry to setup this program (no need to restart computer)
             
         - I'm using Windows 8 (64-bit)
             - Go to '{depsland}/windows_patch/windows-8-64bit' folder
             - Setup 'vcredist-2015-x64.exe'
-            - Retry to setup this program
+            - Retry to setup this program (no need to restart computer)
             
         - I have other problem
             Please contact the supplier or depsland project owner (mail to
