bde7ce51bd078a562aee32a37e940fc4fac76e77
likianta <likianta@foxmail.com>
2022-10-26 17:06:39 +0800

    CLI to init creating manifest file

--- depsland/interface/dev_cli/main.py
+++ depsland/interface/dev_cli/main.py
@@ -1,10 +1,39 @@
+import os
 from argsense import CommandLineInterface
 from lk_utils import dumps
+from lk_utils import fs
 from ... import paths
-from ...profile_reader import get_app_info
 from ...oss import upload as upload_assets
+from ...profile_reader import T
+from ...profile_reader import get_app_info
 
 cli = CommandLineInterface('depsland.dev_cli')
+
+
+@cli.cmd()
+def init(directory: str = '.'):
+    if directory != '.':
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+    if os.path.exists(x := f'{directory}/manifest.json'):
+        r = input(f'target file ({x}) already exists, would you like to '
+                  f'overwrite it? (y/n): ')
+        if r == 'y':
+            os.remove(x)
+        else:
+            print('[dim]no file creates[/]', ':r')
+            return
+    dirpath = fs.normpath(directory, force_abspath=True)
+    dirname = fs.dirname(dirpath)
+    manifest: T.Manifest = {
+        'appid'       : dirname.replace(' ', '_').replace('-', '_').lower(),
+        'name'        : dirname.replace('-', ' ').replace('_', ' ').title(),
+        'version'     : '0.1.0',
+        'assets'      : {},
+        'dependencies': {},
+    }
+    dumps(manifest, x := f'{directory}/manifest.json')
+    print(f'see manifest file at "{x}"')
 
 
 @cli.cmd()
@@ -20,7 +49,7 @@ def upload(manifest_file: str = './manifest.json') -> None:
         upload_assets(
             new_app_dir=appinfo['dst_dir'],
             old_app_dir='{}/{}/{}'.format(
-                paths.apps_dir,
+                paths.Project.apps,
                 appinfo['appid'],
                 appinfo['history'][0]
             )
@@ -28,7 +57,7 @@ def upload(manifest_file: str = './manifest.json') -> None:
     
     appinfo['history'].insert(0, appinfo['version'])
     dumps(appinfo['history'], '{}/{}/released_history.json'.format(
-        paths.apps_dir, appinfo['appid'],
+        paths.Project.apps, appinfo['appid'],
     ))
 
 
