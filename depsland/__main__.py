b0a3e29842a97a6d05ef28ee96e76ccedf921006
likianta <likianta@foxmail.com>
2022-10-31 01:09:52 +0800

    Merge CLI into one (wip)

--- depsland/__main__.py
+++ depsland/__main__.py
@@ -1,6 +1,11 @@
-from argsense import cli
-from .doctor import rebuild_pypi_index
+from argsense import CommandLineInterface
 
+from .doctor import rebuild_pypi_index
+from .interface.dev_cli.cli import init
+
+cli = CommandLineInterface('depsland')
+
+cli.cmd()(init)
 cli.cmd('rebuild-pypi-index')(rebuild_pypi_index)
 
 if __name__ == '__main__':
