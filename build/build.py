fcdb0de4f42b539ba356a825e3830b1c2143fe7c
likianta <likianta@foxmail.com>
2022-11-01 00:06:48 +0800

    Remove legacy build.py

--- build/build.py
+++ build/build.py
@@ -1,119 +1,85 @@
+if 1:
+    import sys
+    from lk_utils import xpath
+    sys.path.insert(0, xpath('..', True))
+
 import os
-import shutil
+from collections import defaultdict
+from os.path import exists
 
-from lk_utils import find_files
+from argsense import cli
+from lk_utils import dumps
+from lk_utils import fs
 
-try:  # importing pyportable-installer
-    # noinspection PyUnresolvedReferences
-    from pyportable_installer import full_build
-except ImportError:
-    try:
-        import sys
-        
-        dir_0 = input('pyportable_installer parent dir: ')
-        sys.path.append(dir_0)
-        dir_1 = input('pyportable_installer related venv dir: ')
-        sys.path.append(f'{dir_1}/lib/site-packages')
-        # noinspection PyUnresolvedReferences
-        from pyportable_installer import full_build
-    except Exception as e:
-        raise e
-    else:
-        del sys
+from depsland import __version__
+from depsland import paths
+from depsland.utils import ziptool
+
+print(':v2', f'depsland version: {__version__}')
 
 
-# ------------------------------------------------------------------------------
-
-def main(full_pack: bool, **kwargs):
-    """
+@cli.cmd()
+def build(add_python_path=True):
+    root_i = paths.project.root
+    root_o = '{dist}/{version}'.format(
+        dist=paths.project.dist,
+        version=f'depsland-{__version__}'
+    )
+    assert not exists(root_o)
+    os.mkdir(root_o)
     
-    Args:
-        full_pack:
-        **kwargs:
-            precheck: bool[True]
-            add_windows_patch: optional[list[int[1, 2, 3, 4]]]
-                None: do not add windows patch
-                1: add windows 7 32bit
-                2: add windows 7 64bit
-                3: add windows 8 32bit
-                4: add windows 8 64bit
-            rename: bool[True]
-    """
-    if kwargs.get('precheck', True):
-        _precheck()
+    # make empty dirs
+    os.mkdir(f'{root_o}/apps')
+    os.mkdir(f'{root_o}/build')
+    os.mkdir(f'{root_o}/conf')
+    # os.mkdir(f'{root_o}/depsland')
+    os.mkdir(f'{root_o}/docs')
+    os.mkdir(f'{root_o}/pypi')
+    os.mkdir(f'{root_o}/pypi/cache')
+    os.mkdir(f'{root_o}/pypi/downloads')
+    os.mkdir(f'{root_o}/pypi/index')
+    os.mkdir(f'{root_o}/pypi/installed')
+    # os.mkdir(f'{root_o}/python')
+    # os.mkdir(f'{root_o}/sidework')
+    os.mkdir(f'{root_o}/temp')
+    os.mkdir(f'{root_o}/temp/.fake_oss_storage')
+    os.mkdir(f'{root_o}/temp/.unittests')
     
-    conf = full_build('pyproject.json')
-    dist_root = conf['build']['dist_dir']
+    # copy files
+    fs.copy_file(f'{root_i}/build/depsland_desktop.exe',
+                 f'{root_o}/build/depsland_desktop.exe')
+    fs.copy_file(f'{root_i}/build/depsland_setup.exe',
+                 f'{root_o}/setup.exe')
+    fs.copy_file(f'{root_i}/build/depsland_setup.py',
+                 f'{root_o}/build/depsland_setup.py')
+    fs.copy_file(f'{root_i}/conf/depsland.yaml',
+                 f'{root_o}/conf/depsland.yaml')
+    fs.copy_file(f'{root_i}/conf/oss_client.yaml',
+                 f'{root_o}/conf/oss_client.yaml')
+    fs.copy_tree(f'{root_i}/depsland',
+                 f'{root_o}/depsland')
+    fs.copy_tree(f'{root_i}/sidework',
+                 f'{root_o}/sidework')
+    if add_python_path:
+        fs.make_link(f'{root_i}/python',
+                     f'{root_o}/python')
     
-    # do not encrypt depsland/doctor.py
-    os.remove(d := f'{dist_root}/src/depsland/doctor.py')
-    shutil.copyfile('../depsland/doctor.py', d)
+    # init files
+    dumps(defaultdict(list), f'{root_o}/pypi/index/dependencies.pkl')
+    dumps(defaultdict(list), f'{root_o}/pypi/index/name_2_versions.pkl')
+    dumps({}, f'{root_o}/pypi/index/name_id_2_paths.pkl')
+    dumps({}, f'{root_o}/pypi/index/updates.pkl')
     
-    _use_custom_setup_launcher(dist_root)
-    
-    _create_embed_python_manager(dist_root, full_assets=full_pack)
-    
-    if x := kwargs.get('add_windows_patch'):
-        _copy_windows_patch(dist_root, x)
-    
-    if kwargs.get('rename', True):
-        if full_pack:
-            os.rename(dist_root, dist_root + '-win64-full')
-        else:
-            os.rename(dist_root, dist_root + '-win64-standard')
+    print(':t', 'see result at', fs.relpath(root_o))
 
 
-def _precheck():
-    if not os.path.exists('./pyportable_runtime'):
-        raise Exception(
-            'couldn\'t find precompiled pyportable_runtime package.',
-            'see `~/docs/steps-to-prebuild-pyportable-runtime-package.zh.md` '
-            'for help.'
-        )
-
-
-def _use_custom_setup_launcher(dist_root: str):
-    # replace `<dist>/setup.exe` with custom setup.bat
-    os.remove(f'{dist_root}/setup.exe')
-    shutil.copyfile('setup_bundle/setup_depsland.bat', f'{dist_root}/setup.bat')
-
-
-def _create_embed_python_manager(dist_root, full_assets: bool):
-    assets = dist_root + '/venv/lib/site-packages/embed_python_manager/assets'
-    if os.path.exists(assets):
-        shutil.rmtree(assets)
-    
-    if full_assets:
-        # about 33MB
-        shutil.copytree('assets/post_build/embed_python_manager/assets', assets)
-    else:
-        os.mkdir(f'{assets}')
-        os.mkdir(f'{assets}/embed_python')
-        os.mkdir(f'{assets}/embed_python/windows')
-        os.mkdir(f'{assets}/pip_suits')
-        os.mkdir(f'{assets}/pip_suits/python2')
-        os.mkdir(f'{assets}/pip_suits/python3')
-        os.mkdir(f'{assets}/tk_suits')
-        os.mkdir(f'{assets}/tk_suits/python2')
-        # os.mkdir(f'{assets}/tk_suits/python3')
-        # add tkinter (about 5MB)
-        shutil.copytree('assets/post_build/embed_python_manager/assets/tk_suits'
-                        '/python3', f'{assets}/tk_suits/python3')
-
-
-def _copy_windows_patch(dist_root: str, select: list):
-    for number, assets_dir in {
-        1: 'windows_patch/windows-7-sp1-32bit',
-        2: 'windows_patch/windows-7-sp1-64bit',
-        3: 'windows_patch/windows-8-32bit',
-        4: 'windows_patch/windows-8-64bit',
-    }.items():
-        if number in select:
-            for fp, fn in find_files(assets_dir, fmt='zip'):
-                fp_i, fp_o = fp, f'{dist_root}/build/{assets_dir}/{fn}'
-                if not os.path.exists(fp_o):
-                    shutil.copyfile(fp_i, fp_o)
+@cli.cmd()
+def compress_to_zip():
+    dir_i = '{}/{}'.format(paths.project.dist, f'depsland-{__version__}')
+    file_o = '{}/{}'.format(paths.project.dist, f'depsland-{__version__}.zip')
+    ziptool.compress_dir(dir_i, file_o, overwrite=True)
+    print(':t', 'see result at', fs.relpath(file_o))
 
 
 if __name__ == '__main__':
-    main(full_pack=True, add_windows_patch=[2])
+    cli.run()
