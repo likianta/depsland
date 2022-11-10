a186cbc3c6dccad57a91fcb616584dd38aae0eee
likianta <likianta@foxmail.com>
2022-11-10 23:00:46 +0800

    Formalize `install_requirements.py`

--- build/install_requirements.py
+++ build/install_requirements.py
@@ -1,15 +1,47 @@
 import os
 
+from argsense import cli
 from lk_utils import run_cmd_args
 
-if os.name == 'nt':
+_IS_WINDOWS = os.name == 'nt'
+
+if _IS_WINDOWS:
     py = 'python/python.exe'
 else:
     py = 'python/bin/python3'
 
-run_cmd_args(
-    py, '-m', 'pip', 'install', '-r', 'requirements.txt',
+pip = (py, '-m', 'pip')
+pip_options = (
     '--find-links', 'chore/custom_packages',
     '--disable-pip-version-check', '--no-warn-script-location',
-    verbose=True
 )
+
+
+@cli.cmd()
+def install_all() -> None:
+    run_cmd_args(
+        pip, 'install', '-r', 'requirements.txt',
+        pip_options, verbose=True
+    )
+    _install_platform_specific()
+
+
+@cli.cmd()
+def install_one(package: str) -> None:
+    run_cmd_args(
+        pip, 'install', package,
+        pip_options, verbose=True
+    )
+
+
+@cli.cmd('install-extra')
+def _install_platform_specific() -> None:
+    if _IS_WINDOWS:
+        run_cmd_args(
+            pip, 'install', 'gen-exe',
+            pip_options, '--no-deps', verbose=True
+        )
+
+
+if __name__ == '__main__':
+    cli.run()
