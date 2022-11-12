b13101ec703ccb6b5a9af13c1698ab1c507054fe
likianta <likianta@foxmail.com>
2022-11-12 16:23:46 +0800

    Show table info

--- depsland/doctor/pypi_index.py
+++ depsland/doctor/pypi_index.py
@@ -1,8 +1,10 @@
 import os
 from collections import defaultdict
 
+from lk_logger.console import con_print
 from lk_utils import dumps
 from lk_utils import fs
+from rich.table import Table
 
 from .. import paths
 from ..pypi import T
@@ -15,6 +17,13 @@ def rebuild_index():
     name_2_versions: T.Name2Versions = defaultdict(list)
     name_id_2_paths: T.NameId2Paths = {}
     updates: T.Updates = {}
+    
+    table = Table()
+    table_index = 0
+    table.add_column('index')
+    table.add_column('filename')
+    table.add_column('name', style='green')
+    table.add_column('version', style='cyan')
     
     def update_name_2_versions():
         name_2_versions[ver.name].append(ver.version)
@@ -37,24 +46,33 @@ def rebuild_index():
         elif utime > updates[name]:
             updates[name] = utime
     
+    def update_table():
+        nonlocal table_index
+        table_index += 1
+        table.add_row(str(table_index), f.name, ver.name, ver.version)
+    
     # -------------------------------------------------------------------------
     
     for f in fs.find_files(paths.pypi.downloads):
         if f.name == '.gitkeep':
             continue
         
-        print(':i', f.name)
         ver = verspec.get_verspec_from_filename(f.name)
         
         update_name_2_versions()
         update_name_id_2_paths()
         update_updates()
+        update_table()
     
     # noinspection PyTypeChecker
     for v in name_2_versions.values():
-        v.sort(key=lambda x: verspec.semver_parse(x.version), reverse=True)
+        v.sort(key=lambda x: verspec.semver_parse(x), reverse=True)
         #   make version list sorted in descending order.
     
     dumps(name_2_versions, paths.pypi.name_2_versions)
     dumps(name_id_2_paths, paths.pypi.name_id_2_paths)
     dumps(updates, paths.pypi.updates)
+    
+    print(':f2')
+    con_print(table)
+    print(':t')
