016e1960915e6196e8490d99094e1dfaab9d1d41
likianta <likianta@foxmail.com>
2022-11-15 01:41:54 +0800

    Fix indexing dependencies

--- depsland/doctor/pypi_index.py
+++ depsland/doctor/pypi_index.py
@@ -6,7 +6,6 @@ from collections import defaultdict
 from lk_logger.console import con_print
 from lk_utils import dumps
 from lk_utils import fs
-from lk_utils import loads
 from rich.table import Table
 
 from .. import normalization as norm
@@ -71,14 +70,14 @@ def rebuild_index(perform_pip_install: bool = False) -> None:
         update_updates()
         update_table()
     
-    # rebuild `dependencies`. this should be called after complete updating
-    # `name_id_2_paths`.
-    dependencies = _rebuild_dependencies()
-    
     # noinspection PyTypeChecker
     for v in name_2_versions.values():
         v.sort(key=lambda x: verspec.semver_parse(x), reverse=True)
         #   make version list sorted in descending order.
+    
+    # rebuild `dependencies`. this should be called after complete updating
+    # `name_id_2_paths`.
+    dependencies = _rebuild_dependencies(name_2_versions)
     
     dumps(name_2_versions, paths.pypi.name_2_versions)
     dumps(name_id_2_paths, paths.pypi.name_id_2_paths)
@@ -110,8 +109,10 @@ class MyTable:
 
 # -----------------------------------------------------------------------------
 
-def _rebuild_dependencies(recursive=True) -> T.Dependencies:
-    name_2_versions: T.Name2Versions = loads(paths.pypi.name_2_versions)
+def _rebuild_dependencies(
+        name_2_versions: T.Name2Versions,
+        recursive=True
+) -> T.Dependencies:
     dependencies: T.Dependencies = {}
     root = paths.pypi.installed
     
@@ -133,51 +134,30 @@ def _rebuild_dependencies(recursive=True) -> T.Dependencies:
                         raise FileNotFoundError(d2.path)  # TODO: for debug
     
     if recursive:
-        def recursively_find_dependencies(
-                source_name_id: str,
-                current_name_id: str,
+        def recursively_find_dependencies_2(
+                name_id: T.NameId,
                 collected: t.Set[T.NameId],
+                indent=0,
         ) -> t.Set[T.NameId]:
-            # nonlocal dependencies
-            nid0 = source_name_id
-            nid1 = current_name_id
-            directs = dependencies[nid0]
-            collected.update(directs)
-            
-            for nid2 in directs:
-                indirects = dependencies[nid2]
-                for nid3 in indirects:
-                    if (
-                            (n0 := nid3 == nid0) or
-                            (n1 := nid3 == nid1)  # noqa
-                    ):
-                        raise Exception(
-                            'circular dependency: '
-                            '[green dim]{}[/] '
-                            '-> [green]{}[/] '
-                            '-> [yellow]{}[/] '
-                            '-> [red u]{}[/] '
-                            '-> [red dim]{}[/]'
-                            .format(nid0, nid1, nid2, nid3,
-                                    nid0 if n0 else nid1)
-                        )
-                    elif nid3 in directs or nid3 in collected:
-                        continue
-                    else:
-                        collected.add(nid3)
-                        recursively_find_dependencies(nid0, nid3, collected)
-            
+            for nid in dependencies[name_id]:
+                if nid not in collected:
+                    print('{}{}'.format(' ' * indent, nid), ':vs')
+                    collected.add(nid)
+                    recursively_find_dependencies_2(nid, collected, indent + 2)
             return collected
         
         old_dependencies = dependencies
         new_dependencies = {}
         for name_id in old_dependencies:
-            new_dependencies[name_id] = recursively_find_dependencies(
-                name_id, name_id, set()
-            )
-        print('recursively find dependencies', '{} -> {}'.format(
-            len(old_dependencies), len(new_dependencies)
-        ))
+            new_dependencies[name_id] = \
+                recursively_find_dependencies_2(name_id, set())
+        print(
+            'recursively find dependencies',
+            'the flatten dependencies have inflated from {} to {}'.format(
+                sum(map(len, old_dependencies.values())),
+                sum(map(len, new_dependencies.values())),
+            ), ':v2'
+        )
         dependencies = new_dependencies
     
     return dependencies
@@ -197,38 +177,38 @@ def _analyse_metadata_1(
     def walk() -> t.Iterator[str]:
         with open(file) as f:
             flag = 0
-            head1 = 'Requires-Dist: '
-            head2 = 'Description-Content-Type: '
+            head = 'Requires-Dist: '
             for line in f:
-                if line.startswith(head2):
-                    break
                 if flag == 0:
-                    if line.startswith(head1):
+                    if line.startswith(head):
                         flag = 1
                     else:
                         continue
                 else:
-                    if line.startswith(head1):
-                        pass
-                    else:
+                    if not line.startswith(head):
                         break
                 # assert flag == 1
-                yield line[len(head1):]
+                yield line[len(head):]
     
     for line in walk():
         if ';' in line:
             # e.g. 'Requires-Dist: toml; extra == "ext"'
             continue
-        raw_name, raw_verspec = pattern.match(line).groups()
+        try:
+            raw_name, raw_verspec = pattern.match(line).groups()
+        except AttributeError as e:
+            print(':v4', file, line)
+            raise e
         name = norm.normalize_name(raw_name)
         verspecs = norm.normalize_version_spec(
             name, raw_verspec or '')
         proper_version = verspec.find_proper_version(
             *verspecs,
-            candidates=name_2_versions.get(name, [])
+            candidates=name_2_versions[name]
         )
         if not proper_version:
-            raise Exception(file, name, raw_verspec)
+            print(':v4l', file, name, raw_verspec, name_2_versions.get(name))
+            raise Exception()
         name_id = f'{name}-{proper_version}'
         yield name_id
 
