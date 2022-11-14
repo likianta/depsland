bcb95347562ca31452407fdb3c737e1dc76a16ab
likianta <likianta@foxmail.com>
2022-11-14 15:51:45 +0800

    Catch failure of uninstalling

--- depsland/api/user_api/uninstall.py
+++ depsland/api/user_api/uninstall.py
@@ -4,16 +4,20 @@ from ... import paths
 
 
 def main(appid: str, version: str, **kwargs) -> None:
-    fs.remove_tree('{}/{}/{}'.format(
-        paths.apps.root, appid, version
-    ))
-    
-    if kwargs.get('remove_venv', True):
-        fs.remove_tree('{}/{}'.format(
-            paths.apps.venv, appid
+    try:
+        fs.remove_tree('{}/{}/{}'.format(
+            paths.apps.root, appid, version
         ))
-    
-    if kwargs.get('remove_bin', True):
-        fs.remove_file('{}/{}.exe'.format(
-            paths.apps.bin, appid
-        ))
+        
+        if kwargs.get('remove_venv', True):
+            fs.remove_tree('{}/{}'.format(
+                paths.apps.venv, appid
+            ))
+        
+        if kwargs.get('remove_bin', True):
+            fs.remove_file('{}/{}.exe'.format(
+                paths.apps.bin, appid
+            ))
+    except PermissionError:  # TODO
+        print('failed to remove old version, '
+              'we will try to delete it again next time', ':v4')
