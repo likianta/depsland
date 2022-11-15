a3ea8bc16f4ed1d748b22ce1cc005648c9d1c050
Likianta <likianta@foxmail.com>
2022-11-15 20:15:22 +0800

    Chore update

--- build/backup_project_resources.py
+++ build/backup_project_resources.py
@@ -1,4 +1,5 @@
 from lk_utils import fs
+from lk_utils import loads
 
 from depsland import paths
 from depsland.utils import make_temp_dir
@@ -9,7 +10,8 @@ dir_o = paths.project.depsland + '/chore'
 
 ziptool.compress_dir(f'{dir_i}/build', f'{dir_o}/build.zip')
 
-# TODO: make sure conf/depsland.yaml has configured local oss.
+# make sure conf/depsland.yaml has configured local oss.
+assert loads(f'{dir_i}/conf/depsland.yaml')['oss']['server'] == 'local'
 dir_x = make_temp_dir()
 fs.copy_file(f'{dir_i}/conf/depsland.yaml', f'{dir_x}/depsland.yaml')
 ziptool.compress_dir(f'{dir_x}/depsland.yaml', f'{dir_o}/conf.zip')
