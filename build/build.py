6dcbb67bb076f633e9f54b01428bff7f45d7bc08
Likianta <likianta@foxmail.com>
2022-12-07 16:35:40 +0800

    Remove deprecated `min_build`

--- build/build.py
+++ build/build.py
@@ -28,9 +28,9 @@ print(':v2', f'depsland version: {__version__}')
 @cli.cmd()
 def full_build(oss_scheme: str, add_python_path=True):
     root_i = paths.project.root
-    root_o = '{dist}/{version}'.format(
+    root_o = '{dist}/depsland-setup-{version}'.format(
         dist=paths.project.dist,
-        version=f'depsland-{__version__}'
+        version=__version__
     )
     assert not exists(root_o)
     os.mkdir(root_o)
@@ -102,73 +102,6 @@ def full_build(oss_scheme: str, add_python_path=True):
     
     print(':t', 'see result at ' + fs.relpath(root_o))
 
-
-@cli.cmd()
-def min_build(add_python_packages=False):  # DELETE
-    root_i = paths.project.root
-    root_o = '{dist}/{version}'.format(
-        dist=paths.project.dist,
-        version=f'depsland-{__version__}-(patch)'
-    )
-    assert not exists(root_o)
-    os.mkdir(root_o)
-    
-    # make empty dirs
-    os.mkdir(f'{root_o}/apps')
-    os.mkdir(f'{root_o}/build')
-    os.mkdir(f'{root_o}/build/exe')
-    os.mkdir(f'{root_o}/conf')
-    # os.mkdir(f'{root_o}/depsland')
-    os.mkdir(f'{root_o}/docs')
-    os.mkdir(f'{root_o}/pypi')
-    os.mkdir(f'{root_o}/pypi/cache')
-    os.mkdir(f'{root_o}/pypi/downloads')
-    os.mkdir(f'{root_o}/pypi/index')
-    os.mkdir(f'{root_o}/pypi/installed')
-    os.mkdir(f'{root_o}/python')
-    # os.mkdir(f'{root_o}/sidework')
-    os.mkdir(f'{root_o}/temp')
-    os.mkdir(f'{root_o}/temp/.fake_oss_storage')
-    os.mkdir(f'{root_o}/temp/.self_upgrade')
-    os.mkdir(f'{root_o}/temp/.unittests')
-    
-    # copy files
-    fs.copy_file(f'{root_i}/build/exe/depsland.exe',
-                 f'{root_o}/build/exe/depsland.exe')
-    fs.copy_file(f'{root_i}/build/exe/desktop.exe',
-                 f'{root_o}/build/exe/desktop.exe')
-    fs.copy_file(f'{root_i}/build/exe/setup_patch.exe',
-                 f'{root_o}/setup.exe')
-    fs.copy_file(f'{root_i}/build/depsland_setup.py',
-                 f'{root_o}/build/depsland_setup.py')
-    fs.copy_file(f'{root_i}/conf/depsland.yaml',
-                 f'{root_o}/conf/depsland.yaml')
-    fs.copy_file(f'{root_i}/conf/oss_client.yaml',
-                 f'{root_o}/conf/oss_client.yaml')
-    fs.copy_tree(f'{root_i}/depsland',
-                 f'{root_o}/depsland')
-    fs.copy_tree(f'{root_i}/sidework',
-                 f'{root_o}/sidework')
-    if add_python_packages:
-        os.mkdir(f'{root_o}/python/Lib')
-        fs.make_link(f'{root_i}/python/Lib/site-packages',
-                     f'{root_o}/python/Lib/site-packages')
-    else:
-        os.mkdir(f'{root_o}/python/Lib')
-        os.mkdir(f'{root_o}/python/Lib/site-packages')
-    
-    # init files
-    dump_manifest(load_manifest(f'{root_i}/manifest.json'),
-                  f'{root_o}/manifest.pkl')
-    dumps(defaultdict(list), f'{root_o}/pypi/index/dependencies.pkl')
-    dumps(defaultdict(list), f'{root_o}/pypi/index/name_2_versions.pkl')
-    dumps({}, f'{root_o}/pypi/index/name_id_2_paths.pkl')
-    dumps({}, f'{root_o}/pypi/index/updates.pkl')
-    
-    print(':t', 'see result at', fs.relpath(root_o))
-
-
-# -----------------------------------------------------------------------------
 
 @cli.cmd()
 def bat_2_exe(file_i: str, show_console=True, uac_admin=False):
