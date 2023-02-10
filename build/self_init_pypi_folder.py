6fc25f7408b15c45eb112468d7b821bf594e9fe2
likianta <likianta@foxmail.com>
2023-02-10 11:48:38 +0800

    Add installing to `self_init_pypi_folder`

--- build/self_init_pypi_folder.py
+++ build/self_init_pypi_folder.py
@@ -7,35 +7,71 @@ requirements:
 from sys import executable
 
 from argsense import cli
+from lk_utils import fs
 from lk_utils import xpath
 from lk_utils.subproc import compose_cmd
 from lk_utils.subproc import run_cmd_args
 
+from depsland import normalization as norm
 from depsland import rebuild_pypi_index
 
 
 @cli.cmd()
-def main(skip_download=False) -> None:
+def main(
+        skip_download=False,
+        skip_install=False,
+        target_root=xpath('../pypi')
+) -> None:
     """
     kwargs:
-        skip_download (-s):
+        skip_download (-sd):
+        skip_install (-si):
     """
     run_cmd_args(executable, '-V')
+    run_cmd_args(executable, '-m', 'pip', '-V')
     if not skip_download:
-        run_cmd_args(executable, '-m', 'pip', '-V')
+        _download(f'{target_root}/downloads')
+    if not skip_install:
+        _install(
+            f'{target_root}/downloads',
+            f'{target_root}/installed',
+        )
+    rebuild_pypi_index()
+
+
+def _download(dir_o: str) -> None:
+    run_cmd_args(
+        *compose_cmd(
+            (executable, '-m', 'pip'),
+            ('download',
+             '-d', dir_o,
+             '-r', xpath('../requirements.txt'),
+             '-f', xpath('../chore/custom_packages'),
+             '-f', dir_o,
+             '-i', 'https://pypi.tuna.tsinghua.edu.cn/simple'),
+        ),
+        verbose=True
+    )
+
+
+def _install(dir_i: str, dir_o: str) -> None:
+    for f in fs.find_files(dir_i):
+        if f.name.startswith('.'): continue
+        name, ver = norm.filename_2_name_version(f.name)
+        d = f'{dir_o}/{name}/{ver}'
+        fs.make_dirs(d)
         run_cmd_args(
             *compose_cmd(
                 (executable, '-m', 'pip'),
-                ('download',
-                 '-d', xpath('../pypi/downloads'),
-                 '-r', xpath('../requirements.txt'),
-                 '-f', xpath('../chore/custom_packages'),
-                 '-f', xpath('../pypi/downloads'),
-                 '-i', 'https://pypi.tuna.tsinghua.edu.cn/simple'),
+                ('install',
+                 f.path,
+                 '-t', d,
+                 '--no-warn-script-location',
+                 '--no-deps',
+                 '--no-index')
             ),
             verbose=True
         )
-    rebuild_pypi_index()
 
 
 if __name__ == '__main__':
