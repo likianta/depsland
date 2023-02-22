ea0ac3d23208b8ab06957cc572fe0f7c094a0da3
Likianta <likianta@foxmail.com>
2023-02-22 11:26:39 +0800

    Full-featured index viewer

--- sidework/index_viewer.py
+++ sidework/index_viewer.py
@@ -1,7 +1,39 @@
-from depsland import pypi
+"""
+usage:
+    if you found the dependencies are not parsed correctly, you can:
+        py sidework/index_viewer.py :false :false :true :false
+"""
+import os
 
-print(':l',
-      pypi.name_2_versions,
-      pypi.name_id_2_paths,
-      pypi.dependencies,
-      pypi.updates)
+from argsense import cli
+
+
+@cli.cmd()
+def main(
+        show_name_2_versions=True,
+        show_name_id_2_paths=True,
+        show_dependencies=True,
+        show_updates=True,
+        pypi_root: str = None,
+):
+    """
+    kwargs:
+        pypi_root (-p):
+    """
+    if pypi_root:
+        os.environ['DEPSLAND_PYPI_PATH'] = pypi_root
+    
+    from depsland import pypi
+    
+    if show_name_2_versions:
+        print(':l', pypi.name_2_versions)
+    if show_name_id_2_paths:
+        print(':l', pypi.name_id_2_paths)
+    if show_dependencies:
+        print(':l', pypi.dependencies)
+    if show_updates:
+        print(':l', pypi.updates)
+
+
+if __name__ == '__main__':
+    cli.run(main)
