749ca8222692aab88bb6ef16f3e1abc3194533eb
likianta <likianta@foxmail.com>
2023-08-14 16:42:11 +0800

    Deduce target type in `depsland init`

--- depsland/api/dev_api/init.py
+++ depsland/api/dev_api/init.py
@@ -7,9 +7,17 @@ from lk_utils import dumps
 from lk_utils import fs
 from lk_utils import loads
 
-from ...manifest import T
+from ...manifest import T as T0
 from ...manifest import init_manifest
 from ...venv.target_venv import get_top_level_package_names
+
+
+class T(T0):
+    # ref: T0.Launcher0
+    TargetInfo = t.TypedDict('TargetInfo', {
+        'name': str,
+        'type': t.Literal['executable', 'module', 'package']
+    })
 
 
 def init(
@@ -37,7 +45,7 @@ def init(
     appid = appname.replace(' ', '_').replace('-', '_').lower()
     print(':v2f2', appname, appid)
     
-    manifest = init_user_manifest(appid, appname)
+    manifest = init_user_manifest(dirpath, appname, appid)
     
     if auto_find_requirements:
         # TODO (2023-08-14): currently only support poetry environment.
@@ -60,10 +68,40 @@ def init(
     print(f'see manifest file at \n\t"{x}"', ':tv2s')
 
 
-def init_user_manifest(appid: str, appname: str) -> T.UserManifest:
+def init_user_manifest(root: str, appname: str, appid: str) -> T.UserManifest:
     manifest: T.Manifest = init_manifest(appid, appname)
     manifest.pop('start_directory')  # noqa
     manifest['version'] = '0.1.0'
     manifest['dependencies']['custom_host'] = []
     manifest['dependencies']['official_host'] = []
+    if x := _deduce_target(root, appid):
+        manifest['launcher']['target'] = x['name']
+        manifest['launcher']['type'] = x['type']
+    else:
+        manifest['launcher']['type'] = 'module'  # as default
     return manifest
+
+
+def _deduce_target(root: str, appid: str) -> t.Optional[T.TargetInfo]:
+    for possible_path in (
+        f'{root}/{appid}',
+        f'{root}/src/{appid}',
+        f'{root}/{appid}.py',
+        f'{root}/src/{appid}.py',
+        f'{root}/{appid}.exe',
+        f'{root}/{appid}',
+    ):
+        if exists(possible_path):
+            if os.path.isdir(possible_path):
+                if exists(f'{possible_path}/__init__.py') and exists(
+                    f'{possible_path}/__main__.py'
+                ):
+                    return {'name': appid, 'type': 'package'}
+            else:
+                if possible_path.endswith('.py'):
+                    return {'name': appid, 'type': 'module'}
+                elif possible_path.endswith('.exe'):
+                    return {'name': appid, 'type': 'executable'}
+                else:
+                    return {'name': appid, 'type': 'executable'}
+    return None
