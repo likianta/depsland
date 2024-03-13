fca232cc05c04d3901e6fe8653f1d8924b191526
likianta <likianta@foxmail.com>
2024-03-13 16:07:03 +0800

    Upgrade dependencies

--- depsland/__init__.py
+++ depsland/__init__.py
@@ -2,13 +2,13 @@ if 1:
     import lk_logger
     lk_logger.setup(quiet=True, show_varnames=True)
     
-if 2:
-    # check python version, if lower than 3.11, import typing_extensions
-    import sys
-    if sys.version_info < (3, 11):
-        import typing
-        from typing_extensions import Self
-        setattr(typing, 'Self', Self)
+# if 2:
+#     # check python version, if lower than 3.11, import typing_extensions
+#     import sys
+#     if sys.version_info < (3, 11):
+#         import typing
+#         from typing_extensions import Self
+#         setattr(typing, 'Self', Self)
 
 from . import api
 from . import config
@@ -28,4 +28,4 @@ from .pypi import pypi
 from .pypi import rebuild_index as rebuild_pypi_index
 
 __version__ = '0.7.0'
-__date__ = '2024-01-10'
+__date__ = '2024-03-13'
