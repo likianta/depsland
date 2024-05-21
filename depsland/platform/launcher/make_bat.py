4857421646e70d9f358bef5503e318bc7201f44d
likianta <likianta@foxmail.com>
2024-05-21 18:39:43 +0800

    Fix bat file template

--- depsland/platform/launcher/make_bat.py
+++ depsland/platform/launcher/make_bat.py
@@ -9,25 +9,38 @@ from ...manifest import T
 
 def make_bat(manifest: T.Manifest, file_o: str, *, debug: bool = False) -> str:
     assert file_o.endswith('.bat')
-    if debug:
-        template = dedent(r'''
-            cd /d %~dp0
-            cd source
-            set PYTHONPATH=.;chore/site_packages
-            .\python\python.exe -m depsland run {appid}
-            pause
-        ''')
-    else:
-        template = dedent(r'''
-            @echo off
-            cd /d %~dp0
-            cd source
-            set PYTHONPATH=.;chore/site_packages
-            .\python\python.exe -m depsland run {appid}
-        ''')
-    script = template.format(
+    # if debug:
+    #     template = dedent(r'''
+    #         cd /d %~dp0
+    #         cd source
+    #         set PYTHONPATH=.;chore/site_packages
+    #         .\python\python.exe -m depsland run {appid}
+    #         pause
+    #     ''')
+    # else:
+    #     template = dedent(r'''
+    #         @echo off
+    #         cd /d %~dp0
+    #         cd source
+    #         set PYTHONPATH=.;chore/site_packages
+    #         .\python\python.exe -m depsland run {appid}
+    #     ''')
+    # script = template.format(
+    #     appid=manifest['appid'],
+    #     version=manifest['version'],
+    # )
+    script = dedent(r'''
+        {echo_off}
+        cd /d %~dp0
+        cd ../../../
+        set PYTHONPATH=.;chore/site_packages
+        set PYTHONUTF8=1
+        .\python\python.exe -m depsland run {appid}
+        {pause}
+    '''.format(
+        echo_off='' if debug else '@echo off',
         appid=manifest['appid'],
-        version=manifest['version'],
-    )
+        pause='pause' if debug else '',
+    ))
     dumps(script, file_o)
     return file_o
