97cc0332095965048981b57a35bda629235c4aed
Likianta <likianta@foxmail.com>
2026-04-27 16:27:28 +0800

    Compact entrance of image converter

--- sidework/image_converter.py
+++ sidework/image_converter.py
@@ -1,7 +1,7 @@
 """
-readme: wiki/launcher-icon-setting.md
 TODO: support svg.
 """
+
 import icnsutil
 import os
 from PIL import Image
@@ -10,27 +10,65 @@ from depsland.utils import make_temp_dir
 from lk_utils import fs
 
 
-@cli.cmd('all')
-def icns_2_all(file_i: str, name_o: str = None) -> None:
+@cli
+def main(file_i: str, file_o: str = '') -> str:
+    """
+    params:
+        file_i (-i):
+        file_o (-o):
+            if not given, use the same directory as file_i.
+            if given directory, use the same filename as file_i.
+            if given path, make sure it ends with '.png' or '.ico'.
+            if given `Literal['.ico', '.png']`, convert it to the according
+            format.
+    """
+    assert file_i.endswith(('.icns', '.png'))
+    if file_o:
+        if file_o in ('.ico', '.png'):
+            file_o = fs.replace_ext(file_i, file_o[1:])
+        elif fs.isdir(file_o):
+            file_o = '{}/{}'.format(file_o, fs.basename(file_i))
+        else:
+            assert file_o.endswith(('.ico', '.png'))
+    else:
+        file_o = fs.replace_ext(file_i, 'ico')
+
+    if file_i.endswith('.icns'):
+        if file_o.endswith('.ico'):
+            icns_2_ico(file_i, file_o)
+        else:
+            icns_2_png(file_i, file_o)
+    else:
+        png_2_ico(file_i, file_o)
+
+    print('result: {}'.format(file_o))
+    return file_o
+
+
+@cli
+def extract_icns(file_i: str, name_o: str = '') -> None:
     assert file_i.endswith('.icns')
-    a, b, c = fs.split(file_i, 3)
-    if name_o is None: name_o = b
+    a, b, c = fs.split(file_i, 3)  # type: ignore
+    if not name_o:
+        name_o = b
     file_png = '{}/{}.png'.format(a, name_o)
     file_ico = '{}/{}.ico'.format(a, name_o)
     icns_2_png(file_i, file_png)
     png_2_ico(file_png, file_ico)
 
 
-@cli.cmd('icns-2-ico')
-def icns_2_ico(file_i: str, file_o: str = None) -> str:
+@cli
+def icns_2_ico(file_i: str, file_o: str = '') -> str:
+    if not file_o:
+        file_o = fs.replace_ext(file_i, 'ico')
     file_m = icns_2_png(file_i, fs.replace_ext(file_i, 'png'))
-    png_2_ico(file_m, file_o or fs.replace_ext(file_i, 'ico'))
+    png_2_ico(file_m, file_o)
     fs.remove_file(file_m)
     return file_o
 
 
-@cli.cmd('icns-2-png')
-def icns_2_png(file_i: str, file_o: str = None) -> str:
+@cli
+def icns_2_png(file_i: str, file_o: str = '') -> str:
     """
     https://pypi.org/project/icnsutil/
     https://blog.csdn.net/qq_34146694/article/details/127251404
@@ -42,10 +80,10 @@ def icns_2_png(file_i: str, file_o: str = None) -> str:
         recursive=True,
         convert_png=True,
     )
-    
+
     results = os.listdir(x)
-    
-    if file_o is None:
+
+    if not file_o:
         file_o = fs.replace_ext(file_i, 'png')
     for possible_name in (
         '256x256.png',
@@ -61,15 +99,17 @@ def icns_2_png(file_i: str, file_o: str = None) -> str:
             break
     else:
         raise FileNotFoundError(results)
-    
+
     return file_o
 
 
-@cli.cmd('png-2-ico')
-def png_2_ico(file_i: str, file_o: str) -> str:
+@cli
+def png_2_ico(file_i: str, file_o: str = '') -> str:
     """
     https://zhuanlan.zhihu.com/p/345770773
     """
+    if not file_o:
+        file_o = fs.replace_ext(file_i, 'ico')
     img = Image.open(file_i).resize((256, 256))
     img.save(file_o, 'ICO')
     return file_o
@@ -77,7 +117,4 @@ def png_2_ico(file_i: str, file_o: str) -> str:
 
 if __name__ == '__main__':
     # pox sidework/image_converter.py -h
-    # pox sidework/image_converter.py all $icns_file
-    # pox sidework/image_converter.py all $icns_file $new_name
-    # pox sidework/image_converter.py icns-2-ico $icns_file
     cli.run()
