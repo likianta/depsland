fc68a255899b62bb27927f8cc4c74e6a343dd065
Likianta <likianta@foxmail.com>
2026-07-16 12:52:33 +0800

    Extra assets rule

--- sidework/patch_maker/patch_maker.py
+++ sidework/patch_maker/patch_maker.py
@@ -15,6 +15,7 @@ from depsland.manifest import load_manifest
 
 class T:
     AbsPath = T0.AbsPath
+    Action = T0.Action
     FileId = str
     RelPath = T0.RelPath
     AssetsDiff = T0.AssetsDiff
@@ -29,7 +30,29 @@ class T:
 
 
 @cli
-def make_patch(old_manifest_file: str, new_manifest_file: str) -> None:
+def make_patch(
+    old_manifest_file: str, new_manifest_file: str, extra_assets: str = ''
+) -> None:
+    """
+    params:
+        extra_assets (-e):
+            pass a path or a semi-colon-separated list of paths.
+            if given path, the format is `@<your_path>`, usually a ".yaml" or -
+            ".json" file. the data structure should be:
+                include:
+                  - <some_relpath>
+                  - ...
+                exclude:
+                  - <some_relpath>
+                  - ...
+            the `include` and `exclude` are optional. make sure at least one -
+            exists.
+            if given semi-colon-separated list, the format is
+            `<relpath1>;<relpath2>;...`. for example:
+                '.depsland/mini_deps/pyarrow;.depsland/mini_deps/pyarrow.libs'
+            the string will be split by semi-colon, it will only be treated as
+            include.
+    """
     old_manifest = load_manifest(old_manifest_file)
     new_manifest = load_manifest(new_manifest_file)
 
@@ -43,14 +66,32 @@ def make_patch(old_manifest_file: str, new_manifest_file: str) -> None:
     # ref: depsland/api/user_api/install.py:_install_files
     diff = diff_manifest(old=old_manifest, new=new_manifest)
 
+    if extra_assets:
+        if extra_assets[0] == '@':
+            x = fs.load(extra_assets[1:])
+            extra_include = frozenset(x.get('include', ()))
+            extra_exclude = frozenset(x.get('exclude', ()))
+        else:
+            extra_include = None
+            extra_exclude = frozenset(extra_assets.split(';'))
+        # print(extra_include, extra_exclude, ':nlv')
+        # return
+    else:
+        extra_include = None
+        extra_exclude = None
+
     _resolve_assets(
         diff['assets'],
         new_manifest['start_directory'],
         '{}/assets'.format(temp_dir),
+        _include=extra_include,
+        _exclude=extra_exclude,
     )
     # _resolve_dependencies(diff['dependencies'], ...)
 
     dump_manifest(new_manifest, fs.here('grocery/manifest.pkl'))
+    #   note: `new_manifest_file` may be json or yaml, but we want pkl format.
+    #   so we use `dump_manifest` instead of `fs.copy_file`.
     exe = _generate_patch_executable(patch_id)
     print(
         'see generated executable: {} ({})'.format(
@@ -61,14 +102,44 @@ def make_patch(old_manifest_file: str, new_manifest_file: str) -> None:
 
 
 def _resolve_assets(
-    assets_diff: T.AssetsDiff, root_i: str, root_o: str
+    assets_diff: T.AssetsDiff,
+    root_i: str,
+    root_o: str,
+    _include: tp.Optional[tp.FrozenSet[T.RelPath]] = None,
+    _exclude: tp.Optional[tp.FrozenSet[T.RelPath]] = None,
 ) -> tp.Tuple[T.AbsPath, T.AbsPath]:
     """
     ref: depsland/api/user_api/install.py:_install_files
     """
     assets_map: T.AssetsMap = {}
-    for action, (relpath, real_relpath), (info0, info1) in assets_diff:
+
+    def transform_action(action: T.Action) -> T.Action:
         if action == 'append' or action == 'update':
+            if _exclude and relpath in _exclude:
+                print('exclude', relpath, ':v7p')
+                return 'ignore'
+            return action
+        elif action == 'delete':
+            if _include and relpath in _include:
+                print('include', relpath, ':v3p')
+                return 'ignore'
+            return 'delete'
+        else:  # 'ignore'
+            if _include and relpath in _include:
+                print('include', relpath, ':v3p')
+                return 'update'
+            return 'ignore'
+
+    for action0, (relpath, real_relpath), (info0, info1) in assets_diff:
+        action1 = transform_action(action0)
+        print(
+            action1
+            if action1 == action0
+            else '{} -> {}'.format(action0, action1),
+            relpath,
+            ':inv',
+        )
+        if action1 == 'append' or action1 == 'update':
             abspath = '{}/{}'.format(root_i, real_relpath)
             assert fs.exist(abspath)
             assets_map[info1.uid] = (
@@ -77,12 +148,12 @@ def _resolve_assets(
                 info1.type == 'dir',
                 True,
             )
-        elif action == 'delete':
+        elif action1 == 'delete':
             assets_map[info0.uid] = (None, relpath, info0.type == 'dir', False)
 
     for file_id, (abspath, relpath, isdir, _) in assets_map.items():
         if abspath:
-            print('add asset to temp dir', relpath, file_id, ':in')
+            print('add', '{} ({})'.format(relpath, file_id), ':iv2')
             fs.make_link(abspath, '{}/{}'.format(root_o, file_id), False)
 
     simplified_assets_map = {
